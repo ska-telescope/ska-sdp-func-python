988b7335b36d5046bc65b816eb6e9d0a7c8ee334
gabicca <gabriella.hodosan@stfc.ac.uk>
2022-11-15 13:34:18 +0000

    ORC-1480: add cupy to optional deps + ad conftest for usage in tests

--- tests/imaging/test_dft_skycomponent_visibility_kernels.py
+++ tests/imaging/test_dft_skycomponent_visibility_kernels.py
@@ -1,145 +1,36 @@
 """
-Unit tests for visibility operations
+Unit tests for DFT kernels
 """
-
-import unittest
-
-import astropy.units as u
 import numpy
-from astropy.coordinates import SkyCoord
-from ska_sdp_datamodels.configuration.config_create import (
-    create_named_configuration,
-)
-from ska_sdp_datamodels.science_data_model.polarisation_model import (
-    PolarisationFrame,
-)
-from ska_sdp_datamodels.sky_model.sky_model import SkyComponent
-from ska_sdp_datamodels.visibility import create_visibility
+import pytest
 
 from ska_sdp_func_python.imaging.dft import dft_skycomponent_visibility
 
 
-class TestVisibilityDFTOperationsKernels(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def init(self, ntimes=2, nchan=10, ncomp=100):
-
-        self.lowcore = create_named_configuration("LOW")
-        self.times = (numpy.pi / 43200.0) * numpy.linspace(0.0, 300.0, ntimes)
-
-        self.frequency = numpy.linspace(1.0e8, 1.1e8, nchan)
-        self.channel_bandwidth = numpy.array(nchan * [1e7 / nchan])
-        self.flux = numpy.array(nchan * [100.0, 20.0, -10.0, 1.0]).reshape(
-            [nchan, 4]
-        )
-
-        # The phase centre is absolute and the component is specified relative (for now).
-        # This means that the component should end up at the position phasecentre+compredirection
-        self.phasecentre = SkyCoord(
-            ra=+180.0 * u.deg, dec=-35.0 * u.deg, frame="icrs", equinox="J2000"
-        )
-        self.compabsdirection = SkyCoord(
-            ra=+181.0 * u.deg, dec=-35.0 * u.deg, frame="icrs", equinox="J2000"
-        )
-        pcof = self.phasecentre.skyoffset_frame()
-        self.compreldirection = self.compabsdirection.transform_to(pcof)
-
-        self.comp = ncomp * [
-            SkyComponent(
-                direction=self.compreldirection,
-                frequency=self.frequency,
-                flux=self.flux,
-            )
-        ]
-
-    def test_dft_stokesiquv_visibility(self):
+@pytest.mark.parametrize(
+    "compute_kernel", ["cpu_looped", "gpu_cupy_raw", "proc_func"]
+)
+def test_dft_stokesiquv_visibility(compute_kernel, vis, comp):
+    """
+    The various DFT kernels return the same results
+    """
+    if compute_kernel == "gpu_cupy_raw":
         try:
             import cupy  # noqa: F401
-
-            compute_kernels = ["cpu_looped", "gpu_cupy_raw", "proc_func"]
         except ModuleNotFoundError:
-            compute_kernels = ["cpu_looped", "proc_func"]
+            return True
 
-        vis = dict()
+    new_vis = vis.copy(deep=True)
+    result = dft_skycomponent_visibility(
+        new_vis,
+        comp,
+        dft_compute_kernel=compute_kernel,
+    )
+    qa = result.visibility_acc.qa_visibility()
+    numpy.testing.assert_almost_equal(qa.data["maxabs"], 2400.0)
+    numpy.testing.assert_almost_equal(qa.data["minabs"], 200.9975124)
+    numpy.testing.assert_almost_equal(qa.data["rms"], 942.9223125)
 
-        self.init(ntimes=2, nchan=10, ncomp=100)
-        for dft_compute_kernel in compute_kernels:
-            # start = time.time()
-            vis[dft_compute_kernel] = create_visibility(
-                self.lowcore,
-                self.times,
-                self.frequency,
-                channel_bandwidth=self.channel_bandwidth,
-                phasecentre=self.phasecentre,
-                weight=1.0,
-                polarisation_frame=PolarisationFrame("linear"),
-            )
-            vis[dft_compute_kernel] = dft_skycomponent_visibility(
-                vis[dft_compute_kernel],
-                self.comp,
-                dft_compute_kernel=dft_compute_kernel,
-            )
-            # vis_size = vis[dft_compute_kernel]["vis"].nbytes / 1024 / 1024 / 1024
-            # print(f"{dft_compute_kernel} {time.time() - start:.3}s Vis size {vis_size:.3}GB")
-            qa = vis[dft_compute_kernel].visibility_acc.qa_visibility()
-            numpy.testing.assert_almost_equal(
-                qa.data["maxabs"], 12000.0000000000
-            )
-            numpy.testing.assert_almost_equal(
-                qa.data["minabs"], 1004.987562112086
-            )
-            numpy.testing.assert_almost_equal(
-                qa.data["rms"], 4714.611562943335
-            )
-
-        # Now check the values of the other kernels against that for 'cpu_looped'
-        if len(compute_kernels) > 1:
-            for dft_compute_kernel in compute_kernels:
-                if dft_compute_kernel != "cpu_looped":
-                    err = numpy.max(
-                        numpy.abs(
-                            vis[dft_compute_kernel]["vis"].data
-                            - vis["cpu_looped"]["vis"].data
-                        )
-                    )
-                    assert err < 1e-12, err
-
-    def test_dft_stokesiquv_visibility_quick(self):
-
-        self.init(ntimes=2, nchan=2, ncomp=2)
-        try:
-            import cupy  # noqa: F401
-
-            compute_kernels = ["gpu_cupy_raw", "cpu_looped", "proc_func"]
-        except ModuleNotFoundError:
-            compute_kernels = ["cpu_looped", "proc_func"]
-
-        vpol = PolarisationFrame("linear")
-        for dft_compute_kernel in compute_kernels:
-            self.vis = create_visibility(
-                self.lowcore,
-                self.times,
-                self.frequency,
-                channel_bandwidth=self.channel_bandwidth,
-                phasecentre=self.phasecentre,
-                weight=1.0,
-                polarisation_frame=vpol,
-            )
-            self.vismodel = dft_skycomponent_visibility(
-                self.vis, self.comp, dft_compute_kernel=dft_compute_kernel
-            )
-            qa = self.vismodel.visibility_acc.qa_visibility()
-            numpy.testing.assert_almost_equal(
-                qa.data["maxabs"], 240.0000000000
-            )
-            numpy.testing.assert_almost_equal(
-                qa.data["minabs"], 20.099751242241776
-            )
-            numpy.testing.assert_almost_equal(
-                qa.data["rms"], 94.29223125886809
-            )
-
-
-if __name__ == "__main__":
-    unittest.main()
+    numpy.testing.assert_almost_equal(
+        result.vis.data.sum(), 16396270.264258286 - 219031.4899462515j
+    )
