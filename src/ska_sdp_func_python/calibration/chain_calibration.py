2504184a69a60979d436ff807005c52090b91be2
chiarasal <chiara.salvoni@cgi.com>
2023-01-30 11:45:07 +0100

    AST-1166: disable linting errors for missing members (false positives)

--- src/ska_sdp_func_python/calibration/chain_calibration.py
+++ src/ska_sdp_func_python/calibration/chain_calibration.py
@@ -359,15 +359,18 @@ def dp3_gaincal(vis, calibration_context, global_solution):
     )
 
     for parset in parset_list:
-        gaincal_step = dp3.make_step(
-            "gaincal", parset, "gaincal.", dp3.MsType.regular
+        gaincal_step = dp3.make_step(  # pylint: disable=E1101
+            "gaincal",
+            parset,
+            "gaincal.",
+            dp3.MsType.regular,  # pylint: disable=E1101
         )
         queue_step = dp3.steps.QueueOutput(parset, "")
         gaincal_step.set_next_step(queue_step)
 
         # DP3 GainCal step assumes 4 polarization are present in the visibility
         nr_correlations = 4
-        dpinfo = dp3.DPInfo(nr_correlations)
+        dpinfo = dp3.DPInfo(nr_correlations)  # pylint: disable=E1101
         dpinfo.set_channels(vis.frequency.data, vis.channel_bandwidth.data)
 
         antenna1 = vis.antenna1.data
@@ -391,7 +394,7 @@ def dp3_gaincal(vis, calibration_context, global_solution):
         queue_step.set_info(dpinfo)
         for time, vis_per_timeslot in calibrated_vis.groupby("time"):
             # Run DP3 GainCal step over each time step
-            dpbuffer = dp3.DPBuffer()
+            dpbuffer = dp3.DPBuffer()  # pylint: disable=E1101
             dpbuffer.set_time(time)
             dpbuffer.set_data(
                 expand_polarizations(
