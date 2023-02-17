6a3433dbf46adf68a6b25b4efd66d534f6c45690
Daniel Mitchell <daniel.mitchell@csiro.au>
2023-02-17 21:57:16 +1100

    YAN-1196: linting fix

--- src/ska_sdp_func_python/calibration/__init__.py
+++ src/ska_sdp_func_python/calibration/__init__.py
@@ -3,9 +3,9 @@
 Calibration of observations, using single Jones matricies
 or chains of Jones matrices.
 """
+from .beamformer_utils import *
 from .chain_calibration import *
 from .dp3_calibration import *
 from .jones import *
 from .operations import *
 from .solvers import *
-from .beamformer_utils import *
